f4832678f04f1168af715fdcfeaeab30fb1be1a1
AndresColognesi <a.cologne9@gmail.com>
2022-10-06 16:07:09 -0300

    feat: Add slippery floor and user input (finish EP03)

--- CubeRace/Assets/PlayerMovement.cs
+++ CubeRace/Assets/PlayerMovement.cs
@@ -2,14 +2,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Public parameters to be populated on the console:
+    //--------------------------------------------------//
+    // Public parameters to be populated on the console //
+    //--------------------------------------------------//
+
+    // Player rigid body:
     public Rigidbody rb;
+
+    // Tunable physics parameters:
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
 
     // Using Fixed update because we are using the Unity Physics system!
     void FixedUpdate() 
     {
         // Add constant force on the z-axis to move player forward:
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        // Player movement triggered by user input:
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce + Time.deltaTime, 0, 0);
+        }
+
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce + Time.deltaTime, 0, 0);
+        }
     }
 }
