caf323c7ed101bdda02c304d8c0d6f45d47a7b8e
AndresColognesi <a.cologne9@gmail.com>
2022-10-06 17:21:54 -0300

    feat: Add collision script to player (finish EP05)

---
+++ CubeRace/Assets/PlayerCollision.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    //--------------------------------------------------//
+    // Public parameters to be populated on the console //
+    //--------------------------------------------------//
+
+    // Referencing the player movement script/component:
+    public PlayerMovement movement;
+
+
+    //----------------//
+    // Custom Methods //
+    //----------------//
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            // Disable player movement script:
+            movement.enabled = false;
+        }
+    }
+}
